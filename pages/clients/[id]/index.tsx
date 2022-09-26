41034632dbc67049d1be7042a03e09d26ec8bede
John Slowik <john@jslowik.com>
2022-09-26 16:51:09 -0500

    Initial link example

--- pages/clients/[id]/index.tsx
+++ pages/clients/[id]/index.tsx
@@ -1,7 +1,12 @@
+import {useRouter} from "next/router";
+
 export default function ClientProjectsPage() {
+    const router = useRouter();
+    console.log(router.query);
+
     return (
         <div>
-            <h1>Projects of agiven client</h1>
+            <h1>Projects of a given client</h1>
         </div>
     );
 }
--- pages/clients/index.tsx
+++ pages/clients/index.tsx
@@ -1,12 +1,32 @@
 import {useRouter} from "next/router";
+import Link from "next/link";
 
 export default function ClientsPage() {
     const router = useRouter();
+    const clients = [
+        {
+            id: "max",
+            name: "Maximilian"
+        },
+        {
+            id: "manu",
+            name: "Manuel"
+        }
+    ]
 
     console.log(router.query);
     return (
         <div>
             <h1>Clients Page</h1>
+            <ul>
+                {clients.map((client) => (
+                    <li key={client.id}>
+                        <Link href={`/clients/${client.id}`}>
+                            {client.name}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
